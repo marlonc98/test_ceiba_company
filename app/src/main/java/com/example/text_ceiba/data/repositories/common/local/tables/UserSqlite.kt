1bb2a91d68c1830684fd67d806d9135fafe30159
marlonc98 <mackiller1998@gmail.com>
2021-12-13 14:26:43 -0500

    sqlite correction

--- app/src/main/java/com/example/text_ceiba/data/repositories/common/local/tables/UserSqlite.kt
+++ app/src/main/java/com/example/text_ceiba/data/repositories/common/local/tables/UserSqlite.kt
@@ -10,13 +10,14 @@ import org.json.JSONObject
 class UserSqlite(private val context: Context) {
     companion object{
         const val TABLE_NAME = "users"
+        const val COLUMN_ID = "id"
         const val COLUMN_NAME = "name"
         const val COLUMN_EMAIL = "email"
         const val COLUMN_PHONE = "phone"
         const val COLUMN_USERNAME = "username"
         fun create ():String {
             return "CREATE TABLE " + TABLE_NAME + " (" +
-                    " $COLUMN_NAME INTEGER PRIMARY KEY," +
+                    " $COLUMN_ID INTEGER PRIMARY KEY," +
                     " $COLUMN_NAME TEXT," +
                     " $COLUMN_EMAIL TEXT," +
                     " $COLUMN_PHONE TEXT," +
